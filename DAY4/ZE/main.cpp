f39e5e996a7687a648a5527316263e5ecddc3f29
tan05 <tan.tantham.tan@gmail.com>
2025-04-24 23:32:35 +0700

    dsa

--- DAY4/ZE/main.cpp
+++ DAY4/ZE/main.cpp
@@ -6,24 +6,13 @@ const int MAX_TYPE = 100005;
 int n;
 
 int main() {
-    
+
     cin >> n;
 
-    int arr[MAX_TYPE] = {0};
-    int lastIndx[MAX_TYPE] = {-1};
+    int a[MAX_TYPE] = {0};
 
     for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-        lastIndx[arr[i]] = i;
-    }
-
-    int minIndex = n;
-    int res = -1;
-
-    for (int i = 0; i < n; i++) {
-        if (lastIndx[arr[i]] == i) {
-            cout << arr[i] << '\n';
-        }
+        cin >> a[i];
     }
 
     return 0;
